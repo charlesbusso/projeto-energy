c8584680bd33e8f682c503cb733dcbbc8fff088f
charles busso <charlesagbusso13@gmail.com>
2023-01-06 18:42:05 -0300

    Ok

--- script.js
+++ script.js
@@ -94,22 +94,21 @@ checkInputs();
 
         formControl.className = "form-control success";
 
-       btnLimpar.addEventListener('click', () =>{
-        formControl.className.removeAttribute('error');
-       });
+       //btnLimpar.addEventListener('click', () =>{
+        
+       
       
 
    
     
-    }
+    
     btnLimpar.addEventListener('click', () =>{
        
-        setErrorFor.remove('small')
-       
+        formControl.className.removeAttribute('small');    
 
 
             
-    };
+    });
  
     function checkEmail(email) {
         return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(

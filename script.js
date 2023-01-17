1b940289501ed735d4433f2e8a8d804f15a2afa9
charles busso <109078429+charlesbusso@users.noreply.github.com>
2023-01-17 08:55:20 -0300

    ok

--- script.js
+++ script.js
@@ -94,21 +94,16 @@ checkInputs();
 
         formControl.className = "form-control success";
 
-       //btnLimpar.addEventListener('click', () =>{
-        
-       
       }
-
+     
+       
+      
+        
+      
    
     
     
-    btnLimpar.addEventListener('click', () =>{
-       
-        formControl.removeAttribute('small');    
-
-
-            
-    });
+   
  
     function checkEmail(email) {
         return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(

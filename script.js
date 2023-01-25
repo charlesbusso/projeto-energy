5c10323e2d22fa058eed4b60626c75ea6f13ad36
charles busso <charlesagbusso13@gmail.com>
2023-01-25 13:50:55 -0300

    Ok

--- script.js
+++ script.js
@@ -110,7 +110,7 @@ checkInputs();
             setErrorFor(email, "");
             setErrorFor(username, "");
             setErrorFor(tel, "");
-            
+            setErrorFor(ic, "");
         }else if(setSuccessFor){
             setSuccessFor("");
        }
